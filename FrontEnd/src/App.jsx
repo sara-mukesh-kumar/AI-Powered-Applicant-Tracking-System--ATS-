752cd9b71414da96811484e8610ff64ab0c33df3
Mohaneswaran-dev <eswaranmohan156@gmail.com>
2026-06-08 17:16:39 +0530

    Applicant Dashboard page Creation

--- FrontEnd/src/App.jsx
+++ FrontEnd/src/App.jsx
@@ -3,18 +3,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import ApplicantDashboard from './components/Applicant/ApplicantDashboard'
+import AdminDashboard from './components/Admin/admindashboard'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <div>Zaalima web development first project AI-Powered Applicant Tracking System (ATS)</div>
+      {/* <h2>Zaalima web development first project AI-Powered Applicant Tracking System (ATS)</h2> */}
+      <ApplicantDashboard />
+      <AdminDashboard />
     </>
+
   )
 }
 
 export default App
+
+
+
 //import RecruiterDashboard from './components/Adim/recruiterdashboard';
 
 //function App() {
--- FrontEnd/src/components/Admin/admindashboard.jsx
+++ FrontEnd/src/components/Admin/admindashboard.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const admindashboard = () => {
   return (
     <div>admindashboard</div>
